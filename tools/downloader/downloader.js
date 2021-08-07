9256db588282dcdd41d8fc43d184c7586452a749
Jose Juan Montes <jjmontes@gmail.com>
2021-08-07 21:10:11 +0200

    Some input validation to tiles downloader tool.

--- tools/downloader/downloader.js
+++ tools/downloader/downloader.js
@@ -1,15 +1,24 @@
+/* eslint-disable no-var */
+
 'use strict'
+
+/**
+ * Retrieves assets and tiles from a remote DDD 3D tiles server.
+ * See readme.md file for more information.
+ */
+
 var http = require('https');
 var fs = require('fs');
-const { error } = require('console');
-const { url } = require('inspector');
+var readlineSync = require('readline-sync');
+//const { error } = require('console');
+//const { url } = require('inspector');
 
 
-const urlsPrueba = ["https://3dsmaps.com/trips/acoruna-menhires.json", "https://3dsmaps.com/trips/salamanca-center.json", "https://3dsmaps.com/trips/newyork-manhattan.json"];
+const MAX_SAFE_URLS = 64
+
 const urls = [
     "https://3dsmaps.com/assets/catalog_materials-default256.glb",
     "https://3dsmaps.com/assets/catalog.glb",
-
     /*
     "https://3dsmaps.com/cache/ddd_http/17/62476/47969.glb",
     "https://3dsmaps.com/cache/ddd_http/17/62476/47968.glb",
@@ -19,32 +28,36 @@ const urls = [
     "https://3dsmaps.com/cache/ddd_http/17/62475/47969.glb"
     */
 ]
+
 const urlPrefix = "https://3dsmaps.com/";
 const urlBase = "https://3dsmaps.com/cache/ddd_http/17/";
 
 
-
-
 var download =  function (url, dest) {
     var file = fs.createWriteStream(dest);
-    
+
     return new Promise ((res, reject) => {
-        var request = http.get(url, function (response) {
+        const request = http.get(url, (response) => {
             response.pipe(file);
-            file.on('finish', function () {
+            file.on('finish', () => {
                 res();  // close() is async, call cb after close completes.
             });
-        }).on('error', function (err) { // Handle errors
+        }).on('error', (err) => { // Handle errors
             fs.unlink(dest); // Delete the file async. (But we don't check the result)
             reject(err);
         });
-        
-    })
+    });
 }
 
-// part of work
 function errorMessage () {
-    console.log("Usage:");
+    process.stdout.write("Usage:\n\n");
+    process.stdout.write("  node downloader.js <output_dir> <min_x,min_y> [max_x,max_y]\n\n");
+    process.stdout.write("Parameters:\n");
+    process.stdout.write("  output_dir    Base output directory for downloaded files\n");
+    process.stdout.write("  min_x,min_y   Tile coordinates to download\n");
+    process.stdout.write("  max_x,max_y   Optional opposite corner of the bounding box of tiles to be downloaded\n");
+    process.stdout.write("\n");
+    process.exit(1);
 }
 
 function divideUrl(url) {
@@ -58,14 +71,14 @@ function divideUrl(url) {
     return [urlWithDirectoryOnly, nameFile];
 }
 
-function addUrl (minXY, maxXY) {
+function addUrls(minXY, maxXY) {
     let split = minXY.split(",");
-    let minX = split[0];
-    let minY = split[1];
-    
+    let minX = parseInt(split[0]);
+    let minY = parseInt(split[1]);
+
     let split2 = maxXY.split(",");
-    let maxX = split2 [0];
-    let maxY = split2 [1];
+    let maxX = parseInt(split2[0]);
+    let maxY = parseInt(split2[1]);
 
     for (let y = minY; y <= maxY; y++) {
         for (let x = minX; x <= maxX; x++) {
@@ -81,12 +94,12 @@ async function workUrls(urls, outputDir) {
         let [urlWithDirectoryOnly, nameFile] = divideUrl(url);
         
         if (checkifIsDownloaded(outputDir + "/" + urlWithDirectoryOnly + "/" + nameFile)) {
-            console.log(`The file has already been downloaded  ${urlWithDirectoryOnly + "/" + nameFile}`);
+            console.log(`File has already been downloaded: ${urlWithDirectoryOnly + "/" + nameFile}`);
             continue;
         }
         
         createFolder(outputDir + "/" + urlWithDirectoryOnly);
-        console.log(`Downloading... ${url} a ${urlWithDirectoryOnly}`);
+        console.log(`Downloading... ${url} to ${outputDir}/${urlWithDirectoryOnly}`);
         await download(url, outputDir + "/" + urlWithDirectoryOnly + "/" + nameFile);
     }
 }
@@ -100,20 +113,29 @@ function createFolder(urlWithDirectoryOnly) {
 }
 
 function main() {
-    const outputDir = process.argv[2]; // 2 pq el 0 es node el 1 es la ruta y el 2 es lo que pones :)
-    const minXY = process.argv[3];
-    const maxXY = process.argv[4];
-    if (process.argv.lenght > 4) {
+
+    // Check input arguments
+    if (process.argv.length < 4 || process.argv.length > 5) {
         errorMessage();
     }
-    addUrl(minXY, maxXY);
+
+    const outputDir = process.argv[2];
+    const minXY = process.argv[3];
+    const maxXY = process.argv.length > 4 ? process.argv[4] : process.argv[3];  // Accept maxXY argument as optional
+
+    addUrls(minXY, maxXY);
+
+    // Sanity check for the number of tiles to be downloaded
+    if (urls.length > MAX_SAFE_URLS) {
+        process.stdout.write(`Warning! You are trying to download many files (${ urls.length } tiles).\n`);
+        const answer = readlineSync.question("Do you wish to continue? Type 'yes' to confirm: ");
+        if (answer !== "yes") {
+            process.exit(2);
+        }
+    }
+
     workUrls(urls, outputDir);
 }
 
 main();
 
-/*
-- Acepta parametros en la linea de comandos que parametros:
-    - Output Dir
-    - Las cordenadas de un cacho
-*/
